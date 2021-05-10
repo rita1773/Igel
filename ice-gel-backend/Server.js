afb64cc8c047b97e8d01661123da3eaf0279d84a
rita1773 <s.ritasharon@gmail.com>
2021-05-10 18:12:00 +0530

    serverjs 1

--- ice-gel-backend/Server.js
+++ ice-gel-backend/Server.js
@@ -35,9 +35,9 @@ app.get('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.use(express.static(path.join(__dirname, '../ice-gel/build')));
 app.get('*', (req, res) =>
